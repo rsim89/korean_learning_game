190e8afd6e3233ceadc49db68038681f6389ccdc
rsim89 <118846661+rsim89@users.noreply.github.com>
2024-10-23 11:49:34 -0400

    Update script.js

--- script.js
+++ script.js
@@ -348,9 +348,8 @@ function resetGame() {
 
 function startCountdown(duration) {
     Swal.fire({
-        const durationInput = document.getElementById('study-duration');
         title: 'Get Ready!',
-        text: `The cards will be hidden after ${durationInput} seconds. Click OK to start the countdown.`,
+        text: `The cards will be hidden after ${duration} seconds. Click OK to start the countdown.`,
         position: 'top', // Position the popup at the top center
         toast: false, // Make it look like a non-blocking popup
         showConfirmButton: true, // Show the confirmation button
